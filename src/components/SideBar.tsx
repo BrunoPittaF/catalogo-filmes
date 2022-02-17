89d7169e33e6271c6bbcbf3ddd23d2578133e896
Caio <caiobrunopittaf@gmail.com>
2022-02-17 10:55:39 -0300

    feat: content componentizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
+import { Button } from "./components/Button";
+import { MovieCard } from "../components/MovieCard";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
 export function SideBar() {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
